f86d1f9fb15cc79cc3db9662502236ecd7a8e11d
arasaka71 <181843709+arasaka71@users.noreply.github.com>
2026-09-06 15:10:30 +0200

    project setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,30 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "4.1.0"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(25))
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:6.0.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-restclient")
+
+    testImplementation(
+        "org.springframework.boot:spring-boot-starter-test"
+    )
+
+    testRuntimeOnly(
+        "org.junit.platform:junit-platform-launcher"
+    )
 }
 
 tasks.test {
